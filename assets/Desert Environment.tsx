697275806154083aa5a7b6f3dada05620c6ddb52
Willson <willsonnovender9@gmail.com>
2026-06-11 01:47:00 +0800

    lvl 2 base template + minor updates on lvl 1 base template

---
+++ assets/Desert Environment.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Desert_environment_tiles_01" tilewidth="16" tileheight="16" tilecount="2128" columns="76">
+ <image source="Desert Pixel Art Environment/Assets/Desert_pixel_art_environment/Textures/Desert_environment_tiles_01.png" width="1216" height="448"/>
+</tileset>
---
+++ assets/Desert Props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Desert_environment_props_01" tilewidth="16" tileheight="16" tilecount="7500" columns="100">
+ <image source="Desert Pixel Art Environment/Assets/Desert_pixel_art_environment/Textures/Desert_environment_props_01.png" width="1600" height="1200"/>
+</tileset>
