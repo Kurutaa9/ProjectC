af067b4bdfeefde7c89a4dada2b575f9faebe640
Willson <willsonnovender9@gmail.com>
2026-06-10 13:57:54 +0800

    level 1 base template

---
+++ assets/Forest Props.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Forest_environment_props_01" tilewidth="16" tileheight="16" tilecount="9300" columns="100">
+ <image source="Forest Pixel Art Environment/Assets/Forest_pixel_art_environment/Textures/Forest_environment_props_01.png" width="1600" height="1500"/>
+</tileset>
