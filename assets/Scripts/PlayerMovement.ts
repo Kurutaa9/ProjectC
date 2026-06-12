eade63db23ed9373380cc8fd507a9ca286925939
Jeffrey Mark Ivanson <jeff@roundbytes.com>
2026-06-12 23:11:29 +0800

    canvas correct

--- assets/Scripts/PlayerMovement.ts
+++ assets/Scripts/PlayerMovement.ts
@@ -7,129 +7,173 @@ export default class PlayerMovement extends cc.Component {
     speed: number = 200;
 
     @property
-    jumpForce: number = 420;
+    jumpForce: number = 450;
 
     @property
     gravity: number = 1200;
-
-    @property
-    useWASD: boolean = true;
 
     private hori: number = 0;
     private velocityY: number = 0;
     private isGrounded: boolean = false;
     private wantJump: boolean = false;
+
     private anim: cc.Animation = null;
 
     onLoad() {
+        cc.director.getCollisionManager().enabled = true;
+
         this.anim = this.getComponent(cc.Animation);
 
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        cc.systemEvent.on(
+            cc.SystemEvent.EventType.KEY_DOWN,
+            this.onKeyDown,
+            this
+        );
+
+        cc.systemEvent.on(
+            cc.SystemEvent.EventType.KEY_UP,
+            this.onKeyUp,
+            this
+        );
     }
 
     onDestroy() {
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
-        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
+        cc.systemEvent.off(
+            cc.SystemEvent.EventType.KEY_DOWN,
+            this.onKeyDown,
+            this
+        );
+
+        cc.systemEvent.off(
+            cc.SystemEvent.EventType.KEY_UP,
+            this.onKeyUp,
+            this
+        );
     }
 
     update(dt: number) {
+
+        // Horizontal movement
         this.node.x += this.hori * this.speed * dt;
 
-        this.velocityY -= this.gravity * dt;
-        this.node.y += this.velocityY * dt;
-
+        // Jump
         if (this.wantJump && this.isGrounded) {
             this.velocityY = this.jumpForce;
             this.isGrounded = false;
             this.playAnim("jump");
         }
 
+        // Gravity
+        this.velocityY -= this.gravity * dt;
+
+        // Vertical movement
+        this.node.y += this.velocityY * dt;
+
         this.wantJump = false;
     }
 
-    private onKeyDown(evt: cc.Event.EventKeyboard) {
-        if (this.useWASD) {
-            if (evt.keyCode === cc.macro.KEY.a) {
+    private onKeyDown(event: cc.Event.EventKeyboard) {
+
+        switch (event.keyCode) {
+
+            case cc.macro.KEY.a:
                 this.hori = -1;
                 this.node.scaleX = -2;
-                if (this.isGrounded) this.playAnim("run");
-            }
 
-            if (evt.keyCode === cc.macro.KEY.d) {
+                if (this.isGrounded) {
+                    this.playAnim("run");
+                }
+                break;
+
+            case cc.macro.KEY.d:
                 this.hori = 1;
                 this.node.scaleX = 2;
-                if (this.isGrounded) this.playAnim("run");
-            }
 
-            if (evt.keyCode === cc.macro.KEY.w) {
+                if (this.isGrounded) {
+                    this.playAnim("run");
+                }
+                break;
+
+            case cc.macro.KEY.w:
                 this.wantJump = true;
-            }
-        } else {
-            if (evt.keyCode === cc.macro.KEY.left) {
-                this.hori = -1;
-                this.node.scaleX = -2;
-                if (this.isGrounded) this.playAnim("run");
-            }
-
-            if (evt.keyCode === cc.macro.KEY.right) {
-                this.hori = 1;
-                this.node.scaleX = 2;
-                if (this.isGrounded) this.playAnim("run");
-            }
-
-            if (evt.keyCode === cc.macro.KEY.up) {
-                this.wantJump = true;
-            }
+                break;
         }
     }
 
-    private onKeyUp(evt: cc.Event.EventKeyboard) {
-        if (
-            evt.keyCode === cc.macro.KEY.a ||
-            evt.keyCode === cc.macro.KEY.d ||
-            evt.keyCode === cc.macro.KEY.left ||
-            evt.keyCode === cc.macro.KEY.right
-        ) {
-            this.hori = 0;
+    private onKeyUp(event: cc.Event.EventKeyboard) {
 
-            if (this.isGrounded) {
-                this.playAnim("idle");
-            }
+        switch (event.keyCode) {
+
+            case cc.macro.KEY.a:
+            case cc.macro.KEY.d:
+
+                this.hori = 0;
+
+                if (this.isGrounded) {
+                    this.playAnim("idle");
+                }
+                break;
         }
     }
 
     onCollisionEnter(other: cc.Collider, self: cc.Collider) {
+
         if (other.node.group === "ground") {
+
             this.isGrounded = true;
             this.velocityY = 0;
+
+            this.node.y =
+                other.node.y +
+                other.node.height / 2 +
+                10;
+
+            if (this.hori === 0) {
+                this.playAnim("idle");
+            } else {
+                this.playAnim("run");
+            }
         }
     }
 
     onCollisionStay(other: cc.Collider, self: cc.Collider) {
+
         if (other.node.group === "ground") {
+
             this.isGrounded = true;
             this.velocityY = 0;
+
+            this.node.y =
+                other.node.y +
+                other.node.height / 2 +
+                10;
         }
     }
 
     onCollisionExit(other: cc.Collider, self: cc.Collider) {
+
         if (other.node.group === "ground") {
             this.isGrounded = false;
         }
     }
 
     private playAnim(state: string) {
+
         if (!this.anim) return;
 
-        if (this.useWASD) {
-            if (state === "idle") this.anim.play("fire_idle");
-            if (state === "run") this.anim.play("fire_run");
-            if (state === "jump") this.anim.play("fire_jump");
-        } else {
-            if (state === "idle") this.anim.play("water_idle");
-            if (state === "run") this.anim.play("water_run");
-            if (state === "jump") this.anim.play("water_jump");
+        switch (state) {
+
+            case "idle":
+                this.anim.play("fire_idle");
+                break;
+
+            case "run":
+                this.anim.play("fire_run");
+                break;
+
+            case "jump":
+                this.anim.play("fire_jump");
+                break;
         }
     }
 }
